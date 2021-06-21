dfc957ed967e1d487fec3ff8d3d6860a7b517e9e
genesis-clone <genesis717@khu.ac.kr>
2021-06-21 17:23:09 +0900

    Employ singleton pattern

--- main.go
+++ main.go
@@ -1,46 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+	"github.com/genesis717-clone/genesiscoin/blockchain"
 )
 
-type block struct {
-	data string
-	hash string
-	prevHash string
-}
-
-type blockChain struct {
-	blocks []block
-}
-
-func (b *blockChain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks) - 1].hash
-	}
-	return ""
-}
-
-func (b *blockChain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockChain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Println(block.data)
-		fmt.Println(block.hash)
-		fmt.Println(block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockChain{}
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-	chain.addBlock("Third Block")
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+	fmt.Println(chain)
 }
