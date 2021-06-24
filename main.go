012df21d1e8df8ad4bd51b2e37a687317be54b58
genesis-clone <genesis717@khu.ac.kr>
2021-06-24 20:39:56 +0900

    Render the html using gohtml file

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/genesis717-clone/genesiscoin/blockchain"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-const port = ":4000"
+const (
+	port = ":4000"
+	templatesDir = "templates/"
+)
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks []*blockchain.Block
+}
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!")
+	data := homeData{"Home", blockchain.GetBlockchain().GetAllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templatesDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templatesDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
