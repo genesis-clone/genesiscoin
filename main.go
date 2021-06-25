fd5bb1eabedc34144d4e37ad806117906ec3d049
genesis-clone <genesis717@khu.ac.kr>
2021-06-26 01:07:47 +0900

    Marshal and Field Tags

--- main.go
+++ main.go
@@ -3,29 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/genesis-clone/genesiscoin/utils"
 	"net/http"
 )
 
 const port = ":4000"
 
 type URLDescription struct {
-	URL         string
-	Method      string
-	Description string
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL: "/",
-			Method: "GET",
+			URL:         "/",
+			Method:      "GET",
 			Description: "See Documentation",
 		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
 	}
-	b, err := json.Marshal(data)
-	utils.HandleErr(err)
-	fmt.Printf("%s", b)
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
 }
 
 func main() {
